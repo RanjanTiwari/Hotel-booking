fdd7a99a5447f5f35cb87d1fd824324f7434a051
Ranjantiwari <Ranjant13@gmail.com>
2022-07-07 12:25:16 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import {
   Routes,
   Route,
-  Link,
   BrowserRouter
 } from "react-router-dom";
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
+import Navbar from '../../components/Navbar/Navbar'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar/>
+      <Header/>
+      </div>
   )
 }
